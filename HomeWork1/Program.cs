efec4823c1aec17dcb0223b06f71580cc036f98f
ViacheslavBoltukhov <mr.boltukhov@mail.ru>
2023-07-21 08:26:38 +0300

    Added task8 in homework

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -43,3 +43,17 @@ int number = Convert.ToInt32(Console.ReadLine());
 if (number%2 == 0) Console.Write("Number is even");
 else Console.Write("Number is not even");
 */
+
+// Task8. Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+/*
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+while (count<=number) 
+{
+    if (count%2 == 0) Console.Write(count + " ");
+    count++;
+}
+*/
