d7469f92626f5e8128b43bfb193cb3bed165acb9
ViacheslavBoltukhov <mr.boltukhov@mail.ru>
2023-07-21 07:51:28 +0300

    Added task2 in  homework

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Урок 1. Знакомство с языком программирования С#
+
+// Task 2. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*
+Console.Write("Input first number: ");
+int firstNum = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second number: ");
+int secondNum = Convert.ToInt32(Console.ReadLine());
+
+if (firstNum>secondNum) Console.Write($"maximum = {firstNum}, minimum = {secondNum}");
+else Console.Write($"maximum = {secondNum}, minimum = {firstNum}");
+*/
+
+// Task 4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
