cff39b25cbdc2501431a69da036c750b9b8a5ba5
ViacheslavBoltukhov <mr.boltukhov@mail.ru>
2023-07-25 09:27:38 +0300

    Added HomeWork2 version 3

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -16,26 +16,20 @@ Console.WriteLine(digit);
 
 // Task 10(Вариант с защитой). Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
 /*
-int SecondDigit(int num)
-{
-    return num / 10 % 10;
-}
-
-int Numb(string messeg)
+int NumbCorrectMeseg()
 {
     Console.Write("Input the three-digit number: ");
     int numb = Convert.ToInt32(Console.ReadLine());
     if (numb < 0) numb = -numb;
-    return numb;
+    if (numb < 100 || numb > 999) 
+    {
+        Console.WriteLine("It's not three-digit number, repeat input number. Input number from 100 to 999 modulo");
+        return NumbCorrectMeseg();
+    }
+    return numb / 10 % 10;
 }
 
-int number = Numb("Input the three-digit number: ");
-while (number < 100 || number > 999) 
-{
-    Console.WriteLine("It's not three-digit number, repeat input number. Input number from 100 to 999 modulo");
-    number = Numb("Input the three-digit number: ");
-}
-int digit = SecondDigit(number);
+int digit = NumbCorrectMeseg();
 Console.WriteLine(digit);
 */
 
@@ -78,25 +72,22 @@ else Console.WriteLine("No, it's not weekend");
 
 // Task 15(Вариант с защитой). Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
 /*
-bool IsWeekend(int num)
-{
-    return num == 6 || num == 7;
-}
-
-int Numb(string messeg)
+bool DayCorrectMeseg()
 {
     Console.Write("Input the number of the day of the week: ");
     int numb = Convert.ToInt32(Console.ReadLine());
-    return numb;
+    if (numb > 0 && numb < 8) 
+    {
+        return numb == 6 || numb == 7;
+    }
+    else
+    {
+        Console.WriteLine("There is no such thing for a week, repeat input of the day of the week. Input number from 1 to 7");
+        return DayCorrectMeseg();
+    }
 }
 
-int numberDay = Numb("Input the number of the day of the week: ");
-while (numberDay < 1 || numberDay > 7) 
-{
-    Console.WriteLine("There is no such thing for a week, repeat input of the day of the week. Input number from 1 to 7");
-    numberDay = Numb("Input the number of the day of the week: ");
-}
-bool weekendDay = IsWeekend(numberDay);
+bool weekendDay = DayCorrectMeseg();
 if (weekendDay) Console.WriteLine("Yes, it's a weekend");
 else Console.WriteLine("No, it's not weekend");
 */
